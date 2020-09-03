8282ca55b6a5a5db6a5df3a4fd8b22a33b7afa86
Geazi Oliveira <geazioliveira@hotmail.com>
2020-09-03 03:39:45 -0300

    teste baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
+let baseURL = '';
+if(process.env.BASE_API){
+   baseURL = process.env.BASE_API; 
+}else{
+   baseURL = 'http://localhost/3005';
+}
+
 export default axios.create({
-  baseURL: process.env.BASE_URL_API || 'http://localhost/3005',
+  baseURL: baseURL,
   headers: {
     'Content-type': 'application/json',
   },
