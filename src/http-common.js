4c6fd985fe22d67e67d88eebc59365655ca7c32b
Geazi Oliveira <geazioliveira@hotmail.com>
2020-09-03 03:29:43 -0300

    teste baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://my-grades-api.herokuapp.com',
+  baseURL: process.env.BASE_URL_API || 'http://localhost/3005',
   headers: {
     'Content-type': 'application/json',
   },
